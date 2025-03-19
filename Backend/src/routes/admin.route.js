892fcb58a4a9b873cde7791d01b9e0a82cd070f1
Adhinraj M <143687830+Adhin07@users.noreply.github.com>
2025-03-19 12:17:45 +0530

    first commit

---
+++ Backend/src/routes/admin.route.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
+import {
+  checkAdmin,
+  createAlbum,
+  createSong,
+  deleteAlbum,
+  deleteSong,
+} from "../controller/admin.controller.js";
+
+const router = Router();
+
+
+//first for all it check below 2 functioin then particular function mentioned
+router.use(protectRoute, requireAdmin);
+
+router.get("/check", checkAdmin);
+
+router.post("/songs", createSong);
+router.delete("/song/:id", deleteSong);
+
+router.post("/albums", createAlbum);
+router.delete("/albums/:id", deleteAlbum);
+
+export default router;
---
+++ Frontend/src/stores/useAuthStore.ts
@@ -0,0 +1,13 @@
+import {create } from 'zustand'
+
+interface AuthStore{
+    isAdmin:boolean;
+    error:string | null
+    isLoading:boolean;
+
+    check
+}
+
+export const useAuthStore =create((set)=>{
+
+})
