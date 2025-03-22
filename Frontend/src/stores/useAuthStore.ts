890f745d656ac6d13939f8246b6c7152a34848c4
Adhinraj M <143687830+Adhin07@users.noreply.github.com>
2025-03-22 11:11:13 +0530

    play button and components

--- Frontend/src/stores/useAuthStore.ts
+++ Frontend/src/stores/useAuthStore.ts
@@ -1,3 +1,4 @@
+import { axiosInstance } from '@/lib/axios';
 import {create } from 'zustand'
 
 interface AuthStore{
@@ -5,9 +6,29 @@ interface AuthStore{
     error:string | null
     isLoading:boolean;
 
-    check
+    checkAdminStatus:()=>Promise<void>;
+    reset:()=> void
 }
 
-export const useAuthStore =create((set)=>{
+export const useAuthStore =create<AuthStore>((set)=>({
+    isAdmin:false,
+    isLoading:false,
+    error:null,
 
-})
+
+    checkAdminStatus:async ()=>{
+        try {
+            const response = await axiosInstance.get('/admin/check')
+            set({isAdmin:response.data.admin})
+        } catch (error:any) {
+            
+            set({isAdmin:false,error:error.response.data.message})
+        }finally{
+            set({isLoading:false})
+        }
+    },
+    
+    reset:()=>{
+        set({isAdmin:false, isLoading:false, error:null})
+    }
+}))
